26f6ed3b044997999139b92967287608b9cbc63b
wal33dahmad <rana.waleed123@gmail.com>
2023-04-19 04:24:04 +0500

    added auth ui

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,9 +4,10 @@ import React, { useCallback, useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar'
 import MenuItem from './MenuItem'
+import useRegisterModal from '@/app/hooks/useRegisterModal'
 
 const UserMenu = () => {
-
+    const registerModal = useRegisterModal()
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleMenu = useCallback(() => {
@@ -31,7 +32,7 @@ const UserMenu = () => {
                     <div className="flex flex-col cursor-pointer">
                         <>
                             <MenuItem onClick={() => { }} label='Login' />
-                            <MenuItem onClick={() => { }} label='Sign up' />
+                            <MenuItem onClick={registerModal.onOpen} label='Sign up' />
                         </>
                     </div>
                 </div>
